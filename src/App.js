606028ff638183f5808e9e25726676d0dfad7aed
zhuj3128 <zhuj3128@uwec.edu>
2020-09-29 00:31:43 -0500

    Update design

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import React from 'react';
 import './App.css';
 
 import {TrackerContainerShortInfo} from'./tracker_container.js';
+
 import {Header} from './tracker_container.js'
 import {SiderLeft} from './tracker_container.js'
-import {SiderRight} from './tracker_container.js'
+import {Name} from './tracker_container.js'
+// import {SiderRight} from './tracker_container.js'
 
 function App() {
   return (
     <div id="meeting_tracker_root">
       <Header/>
       <SiderLeft/>
-      <SiderRight/>
-      <TrackerContainerShortInfo date={{day:"13September,2020",name:"Professor Tan"}}/>
-      <TrackerContainerShortInfo date={{day:"15October,2020",name:"Professor Gurung"}}/>
-      <TrackerContainerShortInfo date={{day:"19November,2020",name:"Professor Rakib"}}/>
-      <TrackerContainerShortInfo date={{day:"19November,2020",name:"Professor Chris"}}/>
+      {/* <SiderRight/> */}
+      {/* <TrackerContainerShortInfo date={{day:"13September,2020",name:"Professor Tan"}}/> */}
+      <Name/>
+      <TrackerContainerShortInfo date={{day:"15October,2020",name:"Past Meetings"}}/>
+      <TrackerContainerShortInfo date={{day:"19November,2020",name:"Current Meeting"}}/>
+      <TrackerContainerShortInfo date={{day:"19November,2020",name:"Future Meetings"}}/>
     </div>
   );
 }
