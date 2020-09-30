45c3fc937ec45680734e4c0318695d9235744307
e <daij5757@uwec.edu>
2020-09-30 21:44:01 +0800

    Introduce Redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './App.css';
-
 import {TrackerContainer} from'./TrackerContainer.js';
-import {SideBar} from './SideBar.js'
+import {SideBar} from './SideBar.js';
 
 function App() {
   return (
@@ -11,7 +10,7 @@ function App() {
         <p>Welcome! Jay</p>
         <div>Logout</div>
       </div>
-      <SideBar type={1}/>
+      <SideBar/>
       <div id="main">
         <div id="title">My Meetings</div>
         <TrackerContainer info={{time:"15 October,2020",location:"Davies Center"}}/>
---
+++ src/reducer.js
@@ -0,0 +1,10 @@
+const intialState={
+    currentPage:0,
+    userType:1,
+};
+
+function reducer(state=intialState,action){
+    return state;
+}
+
+export default reducer;
