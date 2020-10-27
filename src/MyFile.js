f25deae9ce6e3da942f5c5195eed575f3e0045e3
zhuj3128 <zhuj3128@uwec.edu>
2020-10-27 10:51:08 -0500

    update Myfiles

--- src/MyFile.js
+++ src/MyFile.js
@@ -1,10 +1,14 @@
 import React from 'react';
-
+import './css/MyFile.css';
 
 export function MyFile(){
     return(
-        <div>
-
+        <div classname>
+            <div className ="tag-small5">Current File stored</div>
+            <div className ="info-small2">Myresume.txt</div>
+            <div className ="tag-small5">Insert File</div>
+            <div className ="info-small2">upload file</div>
+            <div className ="submitFile-button">Submit</div>
         </div>
     );
 }
