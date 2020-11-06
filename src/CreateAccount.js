4e03d81066ec602c86e2913b5c601ccf42626020
zhuj3128 <zhuj3128@uwec.edu>
2020-11-05 22:15:45 -0600

    check

---
+++ src/CreateAccount.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './css/CreateMeeting.css';
+
+export function CreateAccount(){
+
+    return(
+        <form>
+            <div id = "CreateAccount">
+            
+            </div>
+        </form>
+    );
+}
